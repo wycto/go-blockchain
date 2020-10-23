7cd3f5b9b53dcdd8be40a1583f494c43174342a0
wycto <294287600@qq.com>
2020-10-24 01:18:33 +0800

    学习写的简单区块链，联系go语言

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "wycto/core"
 
 func main() {
 	bc := core.NewBlockchain()
-	bc.SendData("weiyi")
+	bc.SendData("www.wycto.cn")
 	bc.SendData("我爱北京天安门，天安门上太阳升，伟大领袖毛主席，指引我们向前进")
 	bc.Print()
 }
